3e605e9dc04b5d304e460603a967ccfdce740f04
Javier Garcia Ordonez <ordonez@zmt.swiss>
2024-08-28 18:37:51 +0200

    fix GitHub ?

--- .github/dockerhub_login.bash
+++ .github/dockerhub_login.bash
@@ -13,13 +13,13 @@ fi
 
 # check script needed variables
 if [ ! -v OWNER ]; then
-    echo "## ERROR: incorrect usage of CI. OWNER (e.g. dockerhub organization like {{ cookiecutter.default_docker_registry }} or user private name) not defined!"
+    echo "## ERROR: incorrect usage of CI. OWNER (e.g. dockerhub organization like itisfoundation or user private name) not defined!"
     exit 1
 fi
 
-# only upstream is allowed to push to {{ cookiecutter.default_docker_registry }} repo
-if [ "${OWNER,,}" != "{{ cookiecutter.default_docker_registry }}" ] &&\
-    { [ ! -v DOCKER_REGISTRY ] || [ -z "${DOCKER_REGISTRY}" ] || [ "$DOCKER_REGISTRY" = "{{ cookiecutter.default_docker_registry }}" ]; }; then
+# only upstream is allowed to push to itisfoundation repo
+if [ "${OWNER,,}" != "itisfoundation" ] &&\
+    { [ ! -v DOCKER_REGISTRY ] || [ -z "${DOCKER_REGISTRY}" ] || [ "$DOCKER_REGISTRY" = "itisfoundation" ]; }; then
     echo "## ERROR: it is not allowed to push to the main dockerhub repository from a fork!"
     echo "## Please adapt your CI-defined environs (DOCKER_USERNAME, DOCKER_PASSWORD, DOCKER_REGISTRY)"
     exit 1
